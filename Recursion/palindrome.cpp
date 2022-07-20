d0b2c7f1c1dcc29fea359bf930ca9d744337ca6c
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2022-07-20 19:43:57 +0530

    Create palindrome.cpp

---
+++ Recursion/palindrome.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+
+//USING RECURSION
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int helper(string s,int start,int end){
+	    if(start>=end) return 1;
+	    if(s[start]!=s[end]) return 0;
+	    
+	    return helper(s,start+1,end-1);
+	}
+	
+	int isPalindrome(string S)
+	{
+	    int n=S.size()-1;
+	    return helper(S,0,n);
+	}
+
+};
+
+
+
+
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
+
+//MORE OPTIMISED SOLOUTION WITHOUT USING RECURSION
+
+/*//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int isPalindrome(string S)
+	{
+
+		for(int i = 0; i < S.size()/2; i++)
+			if(S[i] != S[S.size() - i - 1])
+				return 0;
+
+		return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}*/
+// } Driver Code Ends
